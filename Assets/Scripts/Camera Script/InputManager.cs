540107d93e204c5daaa5ecb2ac56b920320c265e
dfal0703 <84395375+dfal0703@users.noreply.github.com>
2025-10-28 21:48:08 +0900

    사소한 수정

--- Assets/Scripts/Camera Script/InputManager.cs
+++ Assets/Scripts/Camera Script/InputManager.cs
@@ -7,7 +7,6 @@ public class InputManager : MonoBehaviour
 
     public bool APressed { get; private set; }
     public bool DPressed { get; private set; }
-    public bool SPressed { get; private set; }
 
     public event Action OnWPressed;
     public event Action OnSPressed;
@@ -21,30 +20,21 @@ public class InputManager : MonoBehaviour
 
     void Update()
     {
+        // S키 이벤트
+        if (Input.GetKeyDown(KeyCode.S))
+            OnSPressed?.Invoke();
+
+        // W키 이벤트 (항상 발생)
+        if (Input.GetKeyDown(KeyCode.W))
+            OnWPressed?.Invoke();
+
         // A/D 상태 체크
         bool prevA = APressed;
         bool prevD = DPressed;
         APressed = Input.GetKey(KeyCode.A);
         DPressed = Input.GetKey(KeyCode.D);
+
         if (prevA != APressed || prevD != DPressed)
             OnADChanged?.Invoke();
-
-        // S 상태 체크
-        SPressed = Input.GetKey(KeyCode.S);
-        if (Input.GetKeyDown(KeyCode.S))
-            OnSPressed?.Invoke();
-
-        // W키 이벤트: 반드시 S 상태 + A/D 미입력
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            if (SPressed && !APressed && !DPressed)
-            {
-                OnWPressed?.Invoke();
-            }
-            else
-            {
-                Debug.Log("W 입력 무시됨: S 상태=" + SPressed + ", A=" + APressed + ", D=" + DPressed);
-            }
-        }
     }
 }
